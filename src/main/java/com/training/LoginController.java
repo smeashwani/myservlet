5b1cd7bb58545932597cdbb16ad3bd88a4a06636
smeashwani <104685977+smeashwani@users.noreply.github.com>
2022-09-18 12:08:30 +0530

    18Nov

--- src/main/java/com/training/LoginController.java
+++ src/main/java/com/training/LoginController.java
@@ -2,14 +2,25 @@ package com.training;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Enumeration;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 //@WebServlet("/LoginController")
+@WebServlet(
+		   name="LoginController",
+		   description="Servlet with Annotation",
+		   urlPatterns={"/LoginController","/LoginController1"},
+		   initParams={@WebInitParam(name="param1",value="value1"),
+			       @WebInitParam(name="param2",value="value2")}
+		)
 public class LoginController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
@@ -23,12 +34,34 @@ public class LoginController extends HttpServlet {
 		count++;
 		String name = request.getParameter("name");
 		String password = request.getParameter("password");
-		PrintWriter out = response.getWriter();
-		response.setContentType("text/html");
-		out.print("Welcome to ducat"+"</br>");
-		out.print("name" + name + "</br>");
-		out.print("password" + password + "</br>");
-		out.print("count" + count + "</br>");
+		if(name.equals(password)) {
+			PrintWriter out = response.getWriter();
+			response.setContentType("text/html");
+			out.print("Welcome to ducat"+"</br>");
+			out.print("name" + name + "</br>");
+			out.print("password" + password + "</br>");
+			out.print("count" + count + "</br>");
+			
+			extractConfigInfo(out);
+
+		}else {
+			
+			  RequestDispatcher rd = request.getRequestDispatcher("index.html");
+			  rd.include(request, response);
+			 
+			//response.sendRedirect("index.html");
+		}
 	}
 
+	private void extractConfigInfo(PrintWriter out) {
+		ServletConfig config=getServletConfig(); 
+		Enumeration<String> e=config.getInitParameterNames(); 
+		  
+		String str=""; 
+		while(e.hasMoreElements()){ 
+		str=e.nextElement(); 
+		out.print("<br>Name: "+str); 
+		out.print(" value: "+config.getInitParameter(str)); 
+		}
+	}
 }
