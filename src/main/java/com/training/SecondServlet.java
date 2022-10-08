57059d57c07fdb3b9ce38d921488cbeae95c567f
smeashwani <104685977+smeashwani@users.noreply.github.com>
2022-10-08 11:20:12 +0530

    Listener

--- src/main/java/com/training/SecondServlet.java
+++ src/main/java/com/training/SecondServlet.java
@@ -14,22 +14,21 @@ import javax.servlet.http.HttpSession;
 /**
  * Servlet implementation class FirstServlet
  */
-@WebServlet("/HelloServlet")
-public class HelloServlet extends HttpServlet {
+@WebServlet("/SecondServlet")
+public class SecondServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		response.setContentType("text/html");
-		String txtFieldA = request.getParameter("textFieldA");
-		String txtFieldB = request.getParameter("textFieldB");
 		HttpSession session = request.getSession();
-		session.setAttribute("textFieldA", txtFieldA);
-		session.setAttribute("textFieldB", txtFieldB);
+		String txtFieldA = (String)session.getAttribute("textFieldA");
+		String txtFieldB = (String)session.getAttribute("textFieldB");
 		PrintWriter out = response.getWriter();
 		out.print("A=" + txtFieldA);
 		out.print("B=" + txtFieldB);
-		out.print("<a href='/SecondServlet' >SecondServlet </a>");
+		session.removeAttribute("textFieldA");
+		session.removeAttribute("textFieldB");
 	}
 
 }
