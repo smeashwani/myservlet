287768bffefae86ff0dbc1edd4fd81ee97b80343
smeashwani <104685977+smeashwani@users.noreply.github.com>
2022-10-08 12:21:17 +0530

    refresh

--- src/main/java/com/training/HelloServlet.java
+++ src/main/java/com/training/HelloServlet.java
@@ -2,6 +2,7 @@ package com.training;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.time.Instant;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -26,10 +27,12 @@ public class HelloServlet extends HttpServlet {
 		HttpSession session = request.getSession();
 		session.setAttribute("textFieldA", txtFieldA);
 		session.setAttribute("textFieldB", txtFieldB);
+		response.setIntHeader("refresh", 5);
 		PrintWriter out = response.getWriter();
 		out.print("A=" + txtFieldA);
 		out.print("B=" + txtFieldB);
 		out.print("<a href='/SecondServlet' >SecondServlet </a>");
+		out.print(Instant.now());
 	}
 
 }
